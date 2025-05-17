dc179e59ba0208e98586e7f24889085f01921d5a
cvcalderon <ccalderon@upv.es>
2025-05-17 18:02:54 +0200

    Sharing - Create new FleetManager strategy

--- simfleet/common/lib/fleet/strategies/fleetmanager.py
+++ simfleet/common/lib/fleet/strategies/fleetmanager.py
@@ -8,6 +8,9 @@ from loguru import logger
 
 from simfleet.common.agents.fleetmanager import FleetManagerStrategyBehaviour
 
+from simfleet.communications.protocol import REQUEST_PROTOCOL, REQUEST_PERFORMATIVE, QUERY_PROTOCOL, INFORM_PERFORMATIVE
+from simfleet.utils.status import TRANSPORT_WAITING
+
 
 ################################################################
 #                                                              #
@@ -40,3 +43,53 @@ class DelegateRequestBehaviour(FleetManagerStrategyBehaviour):
                     "Manager sent request to transport {}".format(transport["name"])
                 )
                 await self.send(msg)
+
+
+################################################################
+#                                                              #
+#                     FleetManager Strategy                    #
+#                                                              #
+################################################################
+class SendAvailableTransportsBehaviour(FleetManagerStrategyBehaviour):
+    """
+    Awaits customer's requests and replies with the lest of available transports
+    """
+    async def on_start(self):
+        await super().on_start()
+        self.agent.available_transports = {}
+
+    async def run(self):
+        if not self.agent.registration:
+            await self.send_registration()
+
+        msg = await self.receive(timeout=5)
+        if msg:
+            logger.debug("Manager received message: {}".format(msg))
+            protocol = msg.get_metadata("protocol")
+            if protocol == REQUEST_PROTOCOL:
+                performative = msg.get_metadata("performative")
+                if performative == REQUEST_PERFORMATIVE:  # Message from customer asking for transports
+                    body = json.loads(msg.body)
+                    reply = Message()
+                    content = self.agent.available_transports
+                    reply.to = str(msg.sender)
+                    reply.set_metadata("protocol", QUERY_PROTOCOL)
+                    reply.set_metadata("performative", INFORM_PERFORMATIVE)
+                    reply.body = json.dumps(content)
+                    await self.send(reply)
+                    logger.debug("Fleet manager sent list of transports to customer {}".format(msg.sender))
+
+                # Status message from transport
+                elif performative == INFORM_PERFORMATIVE:
+                    logger.debug(f"FleetManager STATUS MESSAGE received: current transports = {self.agent.available_transports.keys()} ***")
+                    body = json.loads(msg.body)
+                    if body["jid"] in self.agent.available_transports:
+                        # If the transport is not waiting it is not available
+                        if body["status"] != TRANSPORT_WAITING:
+                            del self.agent.available_transports[body["jid"]]
+                            logger.debug(f"DELETED {body['jid']} from available transports: current transports = {self.agent.available_transports.keys()}")
+                    else:
+                        # Store new transport if it is waiting(=available)
+                        if body["status"] == TRANSPORT_WAITING:
+                            self.agent.available_transports[body["jid"]] = body
+                            logger.debug(f"ADDED {body['jid']} to available transports: current transports = {self.agent.available_transports.keys()}")
